d52135103037ce50a171e09b8ba7fa05bb227a90
Edi Pană <edi@Edis-MacBook-Pro.local>
2023-03-19 20:06:57 +0200

    first commit

---
+++ main.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int menu(int &c)
+{
+    cout<<"Choose an option: (1- continue, 0- end)";
+    cin>>c;
+    if(c!=1)
+        return 0;
+    else
+        return 1;
+}
+
+
+bool ok;
+char enunt[100001],raspuns[10][100001];
+int nrcorecte,corecte[7],nrraspunsuri,raspunsuri[4],x,choice,c,nr;
+
+int main()
+{
+    ifstream f("intrebare.txt");
+    ifstream g("raspuns2..txt");
+
+    ofstream gresiteint("gresiteenunt.txt");
+    ofstream gresitenr("gresiteraspunsuri.txt");
+
+    fstream corecteen("corecteenunt.txt",ios::app);
+    fstream corecteras("corecteraspuns.txt",ios::app);
+
+
+
+    while(f.getline(enunt,100001))
+    {
+        ok=true;
+        nr++;
+        cout<<"\033[0;30;45m"<<enunt<<"\033[0m"<<endl<<endl;
+        g>>nrcorecte>>nrraspunsuri;
+
+        for(int i=1;i<=nrraspunsuri;i++)
+        {
+            f.getline(raspuns[i], 100001);
+            cout<<"\033[7;44;30m"<<i<<"\033[0m"<<" "<<raspuns[i]<<endl;
+        }
+
+        for(int i=1;i<=nrcorecte;i++)
+        {
+            g>>x;
+            corecte[i]=x;
+        }
+        for(int i=1;i<=nrcorecte;i++)
+        {
+            cout<<"\033[0;30;47m"<<"Raspunsul dumneavoastra este: "<<"\033[0m";
+            cin>>raspunsuri[i];
+        }
+
+        for(int i=1;i<=nrcorecte;i++)
+        {
+            if(corecte[i]!=raspunsuri[i])
+            {
+                ok=false;
+                if(i==1)
+                    cout<<"\033[1;31mRaspunsul corect este cel cu nr: \033[0m"<<"\033[0;31;40m"<<corecte[i]<<"\033[0m"<<endl;
+                else
+                    cout<<"\033[1;31mCel de-al "<<i<<"-lea raspuns corect este: \033[0m"<<"\033[0;31;40m"<<corecte[i]<<"\033[0m"<<endl;
+
+            }
+        }
+        if(ok==true)
+        {
+            cout<<"Corect!"<<endl;
+            corecteen<<enunt<<endl;
+            for(int i=1;i<=nrraspunsuri;i++)
+                corecteen<<raspuns[i]<<endl;
+            corecteras << nrcorecte << endl << nrraspunsuri << endl;
+            for(int i=1;i<=nrcorecte;i++)
+                corecteras<<corecte[i]<<endl;
+        }
+        cout<<endl;
+
+        ///ai gresit intrebarea
+        if(ok==false)
+        {
+
+            gresiteint << enunt << endl;
+            for (int i = 1; i <= nrraspunsuri; i++)
+                gresiteint << raspuns[i] << endl;
+            gresitenr << nrcorecte << endl << nrraspunsuri << endl;
+            for (int i = 1; i <= nrcorecte; i++)
+                gresitenr << corecte[i] << endl;
+            cout << endl;
+        }
+
+
+
+
+
+        ///calling the menu function
+        if(nr%5==0)
+            if(menu(c)==0)
+            {
+                ///completare probleme gresite
+                ifstream gresiteenre("gresiteenunt.txt");
+                ifstream gresiterasre("gresiteraspunsuri.txt");
+
+                while(f.getline(enunt,100001))
+                    gresiteint<<enunt<<endl;
+                while(g>>x)
+                    gresitenr<<x<<endl;
+
+                ///rescriere fisier initial
+                ///nu mi se rescriu raspunsurile
+                ofstream fout("intrebare.txt");
+                ofstream goutt("raspuns2..txt");
+
+                while(gresiteenre.getline(enunt,100001))
+                    fout<<enunt<<endl;
+                while(gresiterasre>>x)
+                    goutt<<x<<endl;
+
+                return 0;
+            }
+    }
+    f.close();
+    g.close();
+    gresitenr.close();
+    gresiteint.close();
+    return 0;
+
+}
